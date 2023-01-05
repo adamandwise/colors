8669327e4c0f342482d8d77771318d3467e9440c
Adam Wise <wise.adam@student.greenriver.edu>
2023-01-05 16:46:16 -0500

    Added a form where the suer can choose a color

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pick A Color</title>
+
 </head>
 <body>
         <h1>Pick a Color</h1>
+<form method= "post"  acrion="confirm.php">
+    <label><input type="radio" value="red" name="color">Red</label><br>
+    <label><input type="radio" value="blue" name="color">Blue</label><br>
+    <label><input type="radio" value="green" name="color">Green</label><br>
+    <input type="submit" value="Submit">
+</form>
+
+</form>
 </body>
 </html>
